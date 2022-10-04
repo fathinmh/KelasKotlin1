f1a28dc415488230f1e4bf77c1da27c5294b575f
fathinmh <fathinhilmi81@gmail.com>
2022-10-04 15:39:36 +0700

    Latihan

---
+++ src/main/kotlin/DataType.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val dataInt = 5
+    val dataString = "Halo Polban"
+    val dataBoolean = true
+
+    var dataIntVar = 10
+
+    dataInt = 6
+    dataIntVar = 8
+
+    println()
+}
